e6a53dace01bf5e3ab6e167938a0bfcafef3983a
Michael barbarin <michael.barbarin@gmail.com>
2017-05-09 19:20:26 +0200

    fix Grace /

--- Grace.c
+++ Grace.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-//test
 #define namefile "Grace_kid.c"
 #define wr "w"
-char *a="#include <stdio.h>%c%c%ctest%c#define namefile %c%s%c%c#define wr %c%s%c%cchar *a=%c%s%c;%c#define test int main(){FILE *fp; fp=fopen(namefile, wr); fprintf(fp, a, 10, '/', '/', 10, 34, namefile, 34, 10, 34, wr, 34, 10, 34, a, 34, 10, 10, 10);}%ctest%c";
-#define test int main(){FILE *fp; fp=fopen(namefile, wr); fprintf(fp, a, 10, '/', '/', 10, 34, namefile, 34, 10, 34, wr, 34, 10, 34, a, 34, 10, 10, 10);}
+char *a="#include <stdio.h>%c#define namefile %c%s%c%c#define wr %c%s%c%cchar *a=%c%s%c;%c#define test int main(){/*toto*/FILE *fp; fp=fopen(namefile, wr); fprintf(fp, a, 10, 34, namefile, 34, 10, 34, wr, 34, 10, 34, a, 34, 10, 10, 10);}%ctest%c";
+#define test int main(){/*toto*/FILE *fp; fp=fopen(namefile, wr); fprintf(fp, a, 10, 34, namefile, 34, 10, 34, wr, 34, 10, 34, a, 34, 10, 10, 10);}
 test
