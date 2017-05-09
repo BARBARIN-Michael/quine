121737818d2a8cfe7a637f1980d40fd756a8b201
Michael barbarin <michael.barbarin@gmail.com>
2017-05-09 19:20:59 +0200

    Colleen version go

---
+++ Colleen.go
@@ -0,0 +1,5 @@
+package main
+import "fmt"
+/*First comm*/
+const (a="package main%cimport %cfmt%c%c/*First comm*/%cconst (a=%c%s%c)%cfunc main() { /*Second comm*/ fmt.Printf(a, 10, 34, 34, 10, 10, 34, a, 34, 10, 10);}%c")
+func main() { /*Second comm*/ fmt.Printf(a, 10, 34, 34, 10, 10, 34, a, 34, 10, 10);}
