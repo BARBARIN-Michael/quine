1e2de5da3dbc16645099e1915b18b896b41d8d6c
Michael barbarin <michael.barbarin@gmail.com>
2017-05-09 17:11:03 +0200

    fix comments

--- Colleen.c
+++ Colleen.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-//First comm
-char *a="#include <stdio.h>%c%c%cFirst comm%cchar *a=%c%s%c;%cint main(){%c*Second comm*%cprintf(a, 10, '/', '/', 10, 34, a, 34, 10, '/', '/', 10, 10); return 0;}%cvoid secondfunction(){}%c";
+/*First comm*/
+char *a="#include <stdio.h>%c%c*First comm*%c%cchar *a=%c%s%c;%cint main(){%c*Second comm*%cprintf(a, 10, '/', '/', 10, 34, a, 34, 10, '/', '/', 10, 10); return 0;}%cvoid secondfunction(){}%c";
 int main(){/*Second comm*/printf(a, 10, '/', '/', 10, 34, a, 34, 10, '/', '/', 10, 10); return 0;}
 void secondfunction(){}
